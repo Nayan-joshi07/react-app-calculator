744d44c0ca102810b10060e07aeb356aa0d2a664
Nayan joshi <nayan07joshi@gmail.com>
2022-03-19 15:52:11 +0530

    Added the Styled-component
    Screen , Previous and Current

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container,Screen,Previous,Current } from './Styled';
+
+export default function Calculator() {
+    return (
+     <Container> 
+         <Screen>
+             <Previous>10 +</Previous>
+             <Current>10</Current>
+         </Screen>
+     </Container>
+     
+    )
+
+}
